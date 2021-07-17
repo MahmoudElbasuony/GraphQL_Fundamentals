a5247add19477c17f677c798acbeca3acefbf4d7
Mahmoud Elbasuony <mahmoud.elbasuony123@gmail.com>
2021-07-17 21:23:55 +0200

    finailize

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const gql = require("graphql-tag");
-const { ApolloServer } = require("apollo-server");
+const { defaultFieldResolver, GraphQLString } = require("graphql");
+const {
+  ApolloServer,
+  PubSub,
+  SchemaDirectiveVisitor,
+} = require("apollo-server");
+const pubSub = new PubSub();
+const createPostEvent = "Post_Created";
 const users_list = [];
+
+class LogDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const resolver = field.resolve || defaultFieldResolver;
+    field.args.push({
+        type : GraphQLString,
+        name : 'dodo'
+    }); 
+    field.resolve = async (root, ags, ctx, info) => {
+      const result = await resolver.call(this, root, ags, ctx, info);
+      console.log(field.name, this.args, ags);
+      return `${ags.dodo} ` + result + ` ${ags.dodo}`;
+    };
+  }
+}
 const typeDefs = gql`
   # here it is a comment will not displayed in tools
   """
@@ -25,6 +47,7 @@ const typeDefs = gql`
     isAdmin: Boolean!
     type: UserType!
   }
+  directive @log(message: String! = "Default Message") on FIELD_DEFINITION
   enum UserType {
     Admin
     Employee
@@ -33,6 +56,10 @@ const typeDefs = gql`
     Other
     Male
     Female
+  }
+  type Post {
+    text: String! @log(message: "Text here")
+    dep: String @deprecated(reason: "Deprecated")
   }
   type Query {
     user(id: String): IUser
@@ -45,6 +72,11 @@ const typeDefs = gql`
   }
   type Mutation {
     newUser(input: newUserInput!): IUser!
+    createPost(text: String!): Post!
+  }
+
+  type Subscription {
+    newPost: Post
   }
 `;
 
@@ -74,6 +106,16 @@ const resolvers = {
       users_list.push(u);
       return u;
     },
+    createPost(_, { text }) {
+      const post = { text: text + " _ " + Date.now().toString() };
+      pubSub.publish(createPostEvent, { newPost: post });
+      return post;
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: () => pubSub.asyncIterator(createPostEvent),
+    },
   },
   IUser: {
     __resolveType(user) {
@@ -85,7 +127,22 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
+  subscriptions: {
+    onConnect(params) {
+      console.log(params);
+    },
+  },
+  formatError(e) {
+    // or  return formatted errors
+    return e;
+  },
+  schemaDirectives: {
+    log: LogDirective,
+  },
+  context: ({ req, connection }) => {
+    if (connection) {
+      return { ...connection.context };
+    }
     return { data: "Mahmoud" };
   },
 });
